acfd0abde7eba60969182dedc2a0ad3a8900a08b
MrFrigid <Zeyu.Huang0610@gmail.com>
2017-10-20 00:50:27 +0300

    debug

--- Assets/Scripts/GlueToHeadset.cs
+++ Assets/Scripts/GlueToHeadset.cs
@@ -13,12 +13,12 @@ public class GlueToHeadset : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Camera.transform.position);
-		Debug.Log (Camera.transform.forward);
-		Debug.Log (Camera.transform.up);
+		//Debug.Log (Camera.transform.position);
+		//Debug.Log (Camera.transform.forward);
+		//Debug.Log (Camera.transform.up);
 
-		transform.position =  Camera.transform.position+  Camera.transform.forward*3f -  Camera.transform.up;
+		transform.position =  Camera.transform.position+  Camera.transform.forward*2.5f -  Camera.transform.up;
 		transform.rotation = transform.rotation*Camera.transform.rotation;
-		Debug.Log (transform.position);
+		//Debug.Log (transform.position);
 	}
 }
